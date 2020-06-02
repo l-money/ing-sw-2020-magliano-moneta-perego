206dd5a5780afdf3474a6c875a56801087d0b124
Pego43 <gabryperego@hotmail.it>
2020-06-02 22:11:58 +0200

    Update: methods into testTable

--- src/main/java/santorini/view/TestTable.java
+++ src/main/java/santorini/view/TestTable.java
@@ -9,9 +9,13 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import santorini.model.Cell;
 import santorini.model.Gamer;
 import santorini.model.Pawn;
 import santorini.model.Table;
+
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class TestTable {
 
@@ -33,8 +37,7 @@ public class TestTable {
 
     public void initialize() {
         imageView = new ImageView[5][5];
-        c1 = 0;
-        c2 = 0;
+        Scanner input = new Scanner(System.in);
         Table table = new Table();
         Gamer gamer = new Gamer(null, "Pippo", 0, null, null);
         Gamer gamer2 = new Gamer(null, "Pno", 1, null, null);
@@ -52,22 +55,29 @@ public class TestTable {
         p1.setIdGamer(0);
 
         p2.setIdPawn(0);
-        p3.setIdGamer(1);
-        p2.setIdPawn(1);
+        p2.setIdGamer(1);
+        p3.setIdPawn(1);
         p3.setIdGamer(1);
 
         p4.setIdPawn(0);
-        p5.setIdGamer(2);
-        p4.setIdPawn(1);
+        p4.setIdGamer(2);
+        p5.setIdPawn(1);
         p5.setIdGamer(2);
 
         table.setACell(1, 1, 0, false, false, p0);
-        table.setACell(2, 2, 1, false, false, p1);
+        table.setACell(1, 3, 1, false, false, p1);
+
         table.setACell(1, 2, 0, false, false, p2);
         table.setACell(2, 3, 1, false, false, p3);
 
-        initializeCells(table, imageView);
-        myPawns(table, gamer, imageView);
+        table.setACell(0, 0, 3, false, true, null);
+        table.setACell(0, 4, 2, false, true, null);
+        table.setACell(4, 0, 1, false, true, null);
+        table.setACell(4, 4, 0, false, true, null);
+        table.setACell(3, 3, 2, true, false, null);
+        table.setACell(3, 4, 2, true, false, null);
+        printTableStatus(table, imageView);
+        lightMyPawns(table, imageView, gamer.getId());
     }
 
     //Prototipo: Se trova celle con pedine mette immagine di apollo
@@ -104,12 +114,132 @@ public class TestTable {
                             System.out.print("Id pedina: " + t.getTableCell(a, b).getPawn().getIdPawn());
                             System.out.println("\tId Giocatore: " + t.getTableCell(a, b).getPawn().getIdGamer());
                             imageView[a][b + 1].setEffect(lighting);
-
                         }
                     });
 
                 }
             }
+        }
+    }
+
+
+    /**
+     * //TODO: DA SISTEMARE CON LE IMMAGINI APPROPRIATE
+     * Metodo per popolare la table
+     *
+     * @param t     table da gioco
+     * @param image tabella di immagini
+     */
+    public void printTableStatus(Table t, ImageView[][] image) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (t.getTableCell(i, j).isComplete()) {
+                    //mettere immagine con livello + cupola con la seguente sintassi
+                    //"images/.../I_" + t.getTableCell(i,j).getLevel() + "_dome.png"
+                    Image im = new Image(String.valueOf(getClass().getClassLoader().getResource("images/GodCards/Apollo.png")), 58, 53, true, false);
+                    image[i][j] = new ImageView();
+                    image[i][j].setImage(im);
+                    gridPane.add(image[i][j], j, i);
+                } else if (t.getTableCell(i, j).getPawn() != null) {
+                    //mettere immagine con livello + cupola con la seguente sintassi
+                    //"images/.../I_" + t.getTableCell(i,j).getLevel() + "_" + t.getTableCell(i,j).getPawn().getIdGamer()" + "_" + t.getTableCell(i,j).getPawn().getIdPawn() + ".png"
+                    Image im = new Image(String.valueOf(getClass().getClassLoader().getResource("images/GodCards/Ares.png")), 58, 53, true, false);
+                    image[i][j] = new ImageView();
+                    image[i][j].setImage(im);
+                    gridPane.add(image[i][j], j, i);
+                } else if (t.getTableCell(i, j).getLevel() != 0) {
+                    //mettere immagine con solo livello con la seguente sintassi
+                    //"images/.../I_" + t.getTableCell(i,j).getLevel() + ".png"
+                    Image im = new Image(String.valueOf(getClass().getClassLoader().getResource("images/GodCards/Pan.png")), 58, 53, true, false);
+                    image[i][j] = new ImageView();
+                    image[i][j].setImage(im);
+                    gridPane.add(image[i][j], j, i);
+                } else {
+                    //mettere immagine vuota se il livello è 0
+                    Image im = new Image(String.valueOf(getClass().getClassLoader().getResource("images/GodCards/Zeus.png")), 58, 53, true, false);
+                    image[i][j] = new ImageView();
+                    image[i][j].setImage(im);
+                    gridPane.add(image[i][j], j, i);
+                }
+            }
+        }
+    }
+
+    /**
+     * //TODO: DA SISTEMARE
+     * Metodo che mi illumina solo le pedine del giocatore che sta giocando
+     * Quando clicca sulla sua pedina gli si illuminano quelle attorno in cui può muoversi
+     *
+     * @param t       table da gioco
+     * @param image   tabella di immagini
+     * @param idGamer id del giocatore corrente
+     */
+    public void lightMyPawns(Table t, ImageView[][] image, int idGamer) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                Cell cell = t.getTableCell(i, j);
+                if (cell.getPawn() != null && cell.getPawn().getIdGamer() == idGamer && cell.getPawn().getICanPlay()) {
+                    //se: esiste la pedina nella cella && la pedina è della stessa squadra del gamer && la pedina può giocare
+                    //rendere cliccabile e illuminata la cella
+                    image[i][j].setEffect(lighting);
+                    image[i][j].setOnMouseClicked(new EventHandler<MouseEvent>() {
+                        @Override
+                        public void handle(MouseEvent e) {
+                            ImageView im;
+                            im = (ImageView) e.getSource();
+                            System.out.println("Row: " + GridPane.getRowIndex(im));
+                            System.out.println("Column: " + GridPane.getColumnIndex(im));
+                            int a = GridPane.getRowIndex(im);
+                            int b = GridPane.getColumnIndex(im);
+                            System.out.print("Id pedina: " + t.getTableCell(a, b).getPawn().getIdPawn());
+                            System.out.println("\tId Giocatore: " + t.getTableCell(a, b).getPawn().getIdGamer());
+                            image[a][b].setEffect(null);
+                            accessibleCells(t, t.getTableCell(a, b), image);
+                        }
+                    });
+                }
+            }
+        }
+    }
+
+    /**
+     * //TODO: DA SISTEMARE
+     * Metodo che illumina le caselle adiacenti possibili al movimento
+     *
+     * @param t      tavolo da gioco
+     * @param myCell posizione della pedina scelta
+     * @param image  tabella di immagini
+     */
+    //Chiamo il metodo in lightMyPawns
+    public void accessibleCells(Table t, Cell myCell, ImageView[][] image) {
+        ArrayList<Cell> cells = new ArrayList<>();
+        int x = myCell.getX();
+        int y = myCell.getY();
+        for (int i = x - 1; i < x + 2; i++) {
+            for (int j = y - 1; j < y + 2; j++) {
+                //controllo se la casella esiste
+                if (((i != x) || (j != y)) && (i >= 0) && (i <= 4) && (j >= 0) && (j <= 4)) {
+                    //controllo se non c'è la cupola
+                    //TODO potevo fare anche il controllo del livello di salita di livello e di posizione già occupata,
+                    // non posso farlo però, per colpa delle divinità
+                    if ((!t.getTableCell(i, j).isComplete())) {
+                        cells.add(t.getTableCell(i, j));
+                    }
+                }
+            }
+        }
+        for (Cell lightMe : cells) {
+            image[lightMe.getX()][lightMe.getY()].setEffect(lighting);
+            image[lightMe.getX()][lightMe.getY()].setOnMouseClicked(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent e) {
+                    ImageView im;
+                    im = (ImageView) e.getSource();
+                    System.out.println("Row: " + GridPane.getRowIndex(im));
+                    System.out.println("Column: " + GridPane.getColumnIndex(im));
+                }
+            });
+
         }
     }
 
