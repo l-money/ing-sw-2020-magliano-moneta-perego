82cd45e078dad4d008c5b3c49c42a4155c79543e
Pego43 <gabryperego@hotmail.it>
2020-06-06 19:36:56 +0200

    Update: printTable with buttons

--- src/main/java/santorini/view/TestTable.java
+++ src/main/java/santorini/view/TestTable.java
@@ -2,12 +2,16 @@ package santorini.view;
 
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.geometry.HPos;
+import javafx.geometry.Pos;
+import javafx.geometry.VPos;
+import javafx.scene.control.Button;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.effect.Lighting;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import santorini.model.Cell;
 import santorini.model.Gamer;
@@ -21,7 +25,8 @@ public class TestTable {
 
     @FXML
     private GridPane gridPane;
-    private ImageView[][] imageView;
+    //private ImageView[][] imageView;
+    private Button[][] buttons;
     private Stage stage;
     private int c1, c2;
     private Lighting lighting = new Lighting();
@@ -36,7 +41,8 @@ public class TestTable {
     // Spostamento pedina con secondo click
 
     public void initialize() {
-        imageView = new ImageView[5][5];
+        //imageView = new ImageView[5][5];
+        buttons = new Button[5][5];
         Scanner input = new Scanner(System.in);
         Table table = new Table();
         Gamer gamer = new Gamer(null, "Pippo", 0, null, null);
@@ -76,8 +82,10 @@ public class TestTable {
         table.setACell(4, 4, 0, false, true, null);
         table.setACell(3, 3, 2, true, false, null);
         table.setACell(3, 4, 2, true, false, null);
-        printTableStatus(table, imageView);
-        lightMyPawns(table, imageView, gamer.getId());
+        printTableStatusB(table, buttons);
+        lightMyPawnsB(table, buttons, gamer.getId());
+        //printTableStatus(table, imageView);
+        //lightMyPawns(table, imageView, gamer.getId());
     }
 
     //Prototipo: Se trova celle con pedine mette immagine di apollo
@@ -129,6 +137,7 @@ public class TestTable {
      *
      * @param t     table da gioco
      * @param image tabella di immagini
+     *              ImageView[][] image
      */
     public void printTableStatus(Table t, ImageView[][] image) {
         for (int i = 0; i < 5; i++) {
@@ -160,6 +169,67 @@ public class TestTable {
                     image[i][j] = new ImageView();
                     image[i][j].setImage(im);
                     gridPane.add(image[i][j], j, i);
+                }
+            }
+        }
+    }
+
+    public void printTableStatusB(Table t, Button[][] bt) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (t.getTableCell(i, j).isComplete()) {
+                    //mettere immagine con livello + cupola con la seguente sintassi
+                    //"images/.../I_" + t.getTableCell(i,j).getLevel() + "_dome.png"
+                    //Image im = new Image(String.valueOf(getClass().getClassLoader().getResource("images/GodCards/Apollo.png")), 58, 53, true, false);
+                    bt[i][j] = new Button();
+                    BackgroundImage myBI = new BackgroundImage(new Image(String.valueOf(getClass().getClassLoader().getResource("images/GodCards/Apollo.png")), 58, 53, true, false),
+                            BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER,
+                            BackgroundSize.DEFAULT);
+                    bt[i][j].setBackground(new Background(myBI));
+                    bt[i][j].setMaxSize(60, 60);
+                    gridPane.add(bt[i][j], j, i);
+                    gridPane.setHalignment(bt[i][j], HPos.CENTER);
+                    gridPane.setValignment(bt[i][j], VPos.CENTER);
+                } else if (t.getTableCell(i, j).getPawn() != null) {
+                    //mettere immagine con livello + cupola con la seguente sintassi
+                    //"images/.../I_" + t.getTableCell(i,j).getLevel() + "_" + t.getTableCell(i,j).getPawn().getIdGamer()" + "_" + t.getTableCell(i,j).getPawn().getIdPawn() + ".png"
+                    Image im = new Image(String.valueOf(getClass().getClassLoader().getResource("images/GodCards/Ares.png")), 58, 53, true, false);
+                    bt[i][j] = new Button();
+                    BackgroundImage myBI = new BackgroundImage(im,
+                            BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER,
+                            BackgroundSize.DEFAULT);
+                    bt[i][j].setBackground(new Background(myBI));
+                    bt[i][j].setMaxSize(60, 60);
+                    bt[i][j].setMinSize(60, 60);
+                    gridPane.add(bt[i][j], j, i);
+                    gridPane.setHalignment(bt[i][j], HPos.CENTER);
+                    gridPane.setValignment(bt[i][j], VPos.CENTER);
+                } else if (t.getTableCell(i, j).getLevel() != 0) {
+                    //mettere immagine con solo livello con la seguente sintassi
+                    //"images/.../I_" + t.getTableCell(i,j).getLevel() + ".png"
+                    Image im = new Image(String.valueOf(getClass().getClassLoader().getResource("images/GodCards/Pan.png")), 58, 53, true, false);
+                    BackgroundImage myBI = new BackgroundImage(im,
+                            BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER,
+                            BackgroundSize.DEFAULT);
+                    bt[i][j] = new Button();
+                    bt[i][j].setBackground(new Background(myBI));
+                    bt[i][j].setMaxSize(60, 60);
+                    bt[i][j].setMinSize(60, 60);
+                    gridPane.add(bt[i][j], j, i);
+                    gridPane.setHalignment(bt[i][j], HPos.CENTER);
+                    gridPane.setValignment(bt[i][j], VPos.CENTER);
+                } else {
+                    //mettere immagine vuota se il livello è 0
+                    bt[i][j] = new Button();
+                    bt[i][j].setAlignment(Pos.BOTTOM_CENTER);
+                    bt[i][j].setBackground(Background.EMPTY);
+                    bt[i][j].setMaxSize(60, 60);
+                    bt[i][j].setMinSize(60, 60);
+                    gridPane.add(bt[i][j], j, i);
+                    gridPane.setHalignment(bt[i][j], HPos.CENTER);
+                    gridPane.setValignment(bt[i][j], VPos.CENTER);
+
+
                 }
             }
         }
@@ -237,6 +307,67 @@ public class TestTable {
                     im = (ImageView) e.getSource();
                     System.out.println("Row: " + GridPane.getRowIndex(im));
                     System.out.println("Column: " + GridPane.getColumnIndex(im));
+                }
+            });
+
+        }
+    }
+
+    public void lightMyPawnsB(Table t, Button[][] bt, int idGamer) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                Cell cell = t.getTableCell(i, j);
+                if (cell.getPawn() != null && cell.getPawn().getIdGamer() == idGamer && cell.getPawn().getICanPlay()) {
+                    //se: esiste la pedina nella cella && la pedina è della stessa squadra del gamer && la pedina può giocare
+                    //rendere cliccabile e illuminata la cella
+                    bt[i][j].setEffect(lighting);
+                    bt[i][j].setOnMouseClicked(new EventHandler<MouseEvent>() {
+                        @Override
+                        public void handle(MouseEvent e) {
+                            Button b;
+                            b = (Button) e.getSource();
+                            System.out.println("Row: " + GridPane.getRowIndex(b));
+                            System.out.println("Column: " + GridPane.getColumnIndex(b));
+                            int x = GridPane.getRowIndex(b);
+                            int y = GridPane.getColumnIndex(b);
+                            System.out.print("Id pedina: " + t.getTableCell(x, y).getPawn().getIdPawn());
+                            System.out.println("\tId Giocatore: " + t.getTableCell(x, y).getPawn().getIdGamer());
+                            bt[x][y].setEffect(null);
+                            accessibleCellsB(t, t.getTableCell(x, y), bt);
+                        }
+                    });
+                }
+            }
+        }
+    }
+
+    public void accessibleCellsB(Table t, Cell myCell, Button[][] bt) {
+        ArrayList<Cell> cells = new ArrayList<>();
+        int x = myCell.getX();
+        int y = myCell.getY();
+        for (int i = x - 1; i < x + 2; i++) {
+            for (int j = y - 1; j < y + 2; j++) {
+                //controllo se la casella esiste
+                if (((i != x) || (j != y)) && (i >= 0) && (i <= 4) && (j >= 0) && (j <= 4)) {
+                    //controllo se non c'è la cupola
+                    //TODO potevo fare anche il controllo del livello di salita di livello e di posizione già occupata,
+                    // non posso farlo però, per colpa delle divinità
+                    if ((!t.getTableCell(i, j).isComplete())) {
+                        cells.add(t.getTableCell(i, j));
+                    }
+                }
+            }
+        }
+        for (Cell lightMe : cells) {
+            bt[lightMe.getX()][lightMe.getY()].setStyle("-fx-border-color:yellow");
+            //bt[lightMe.getX()][lightMe.getY()].setEffect(lighting);
+            bt[lightMe.getX()][lightMe.getY()].setOnMouseClicked(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent e) {
+                    Button b;
+                    b = (Button) e.getSource();
+                    System.out.println("Row: " + GridPane.getRowIndex(b));
+                    System.out.println("Column: " + GridPane.getColumnIndex(b));
                 }
             });
 
